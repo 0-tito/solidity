a368da7f07228c54b52d6f2145acae7cfe0b7c95
michael <michaelisaac372@gmaail.com>
2025-08-27 06:08:03 -0700

    initial commit

--- deploy.js
+++ deploy.js
@@ -18,7 +18,8 @@ const deploy = async () =>  {
   const result = await new web3.eth.Contract(JSON.parse(interface))
    .deploy({data : bytecode})
    .send({ gas: `1000000`, from: accounts[0]})
-  console.log(interface)
+
+  console.log("interface:", interface)
   console.log(`contract was deployed to`, result.options.address)
-}  
+}
 deploy()
