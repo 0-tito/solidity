bf57ef040e5e8c1843ad8245b6ea33b0c8d4dd59
michael <michaelisaac372@gmaail.com>
2025-08-30 10:23:06 -0700

    update contract code to version 0.8.9, update compile.js to add the expected JSON formatted input, specifying the language,sources,outputSelection update deploy.js to use the newer truffle module, update import to destructure the abi and evm call provider.engine.stop() to prevent deployment from hanging in the terminal

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { Web3 } = require("web3");
+const { abi, evm } = require("./compile");
+
+const provider = new HDWalletProvider(
+     'multiply airport grace lumber six apple bean spot hope orbit ahead car',
+    'https://sepolia.infura.io/v3/8e37cda2efbb43c5b9fe80ac05933711'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log(JSON.stringify(abi));
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop()
+};
+deploy();
